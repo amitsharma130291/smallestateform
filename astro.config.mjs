fdf536e347e31ea434dc4dfec416d247e195a97e
Amit <amit@smallestateform.com>
2026-07-28 22:31:55 +0000

    feat: OG tags, sitemap integration, fix footer /privacy/ link

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   site: 'https://smallestateform.com',
-  integrations: [react(), tailwind({ config: './tailwind.config.mjs' })],
+  integrations: [react(), sitemap(), tailwind({ config: './tailwind.config.mjs' })],
   output: 'hybrid',
   adapter: vercel(),
 });
